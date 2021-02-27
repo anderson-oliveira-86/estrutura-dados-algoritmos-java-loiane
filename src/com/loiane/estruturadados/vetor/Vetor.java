c72ff262ac0d65057a68392a4930b15963cd8e30
Anderson de Oliveira <anderson.o.oliveira@hotmail.com>
2021-02-27 11:48:27 -0300

    Primeiro Commit

---
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -0,0 +1,60 @@
+package com.loiane.estruturadados.vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+    private int tamanho;
+
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    public void adiciona(String elemento) {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        } else {
+            throw new IllegalArgumentException("Vetor já está cheio, não é possível adicionar mais elementos");
+        }
+    }
+
+    public String busca(int posicao) {
+        if (!(posicao >= 0 && posicao > tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        return this.elementos[posicao];
+    }
+
+    public int busca(String elemento) {
+        for (int i = 0; i < this.tamanho; i++) {
+            if (elemento.equals(elementos[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+        }
+        s.append("]");
+        return s.toString();
+    }
+}
